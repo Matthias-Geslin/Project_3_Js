3af5c0259e12493013867d376c68d686d0a55599
Matthias <46106190+Matthias-Geslin@users.noreply.github.com>
2019-05-06 13:40:19 +0200

    Update-002
    Add Objects Js files

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+document.addEventListener('DOMContentLoaded', function (e) {
+    // Create and launch the slider
+    var slider = new Slider();
+    slider.onSliderNext();
+
+    // Create and launch the map
+    var map = new Map();
+    map.mapInit();
+})
