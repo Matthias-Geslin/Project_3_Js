b235079fbe85a8dd59ca76f98aa4fe45ce59fd24
Matthias <m.geslin974@gmail.com>
2019-07-08 09:51:51 +0200

    edit on single quotes to double

--- js/Map.js
+++ js/Map.js
@@ -1,43 +1,35 @@
-'use strict';
+"use strict";
 
 var Map = function () {
 }
 
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 13,
         center: {lat: 47.2173, lng: -1.5534}
     });
 
     ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Nantes&apiKey=58866263d27fa1a6d54be4b8a79069f811cd87fd",
         function (reponse) {
-            var stations = JSON.parse(reponse);
-            var lattitude, longitude;
-
-            stations.forEach(function (station) {
-                lattitude = station.position.lat;
-                longitude = station.position.lng;
-                addMarker(station);
-            })
 
             // Add Marker Function
             function addMarker(station) {
                 var marker = new google.maps.Marker({
                     position: station.position,
                     map: map,
-                    icon:''
+                    icon:""
                 });
 
-                let stationStatus = document.getElementById('station-status');
+                let stationStatus = document.getElementById("station-status");
                 // Check if station Open
                 if (station.status === "OPEN") {
                     marker.icon= {
-                        url: 'img/green.png',
+                        url: "img/green.png",
                     };
                     stationStatus.textContent = "Ouverte";
                 } else {
                     marker.icon= {
-                        url:'img/red.png'
+                        url:"img/red.png"
                     };
                     stationStatus.textContent = "Fermée";
                 }
@@ -49,17 +41,17 @@ function initMap() {
                     var infoWindow = new google.maps.InfoWindow({
                         content: station.name
                     });
-                    const reserve = document.getElementById('reservation-box');
-                    marker.addListener('click', function () {
+                    const reserve = document.getElementById("reservation-box");
+                    marker.addListener("click", function () {
 
-                        reserve.classList.remove('hide');
-                        reserve.classList.add('flex');
+                        reserve.classList.remove("hide");
+                        reserve.classList.add("flex");
 
-                        const stationName = document.getElementById('station-name');
-                        const stationAddress = document.getElementById('station-address');
-                        const bikeStands = document.getElementById('station-stands');
-                        const availableBikes = document.getElementById('available-bikes');
-                        const buttonReservation = document.getElementById('validate');
+                        const stationName = document.getElementById("station-name");
+                        const stationAddress = document.getElementById("station-address");
+                        const bikeStands = document.getElementById("station-stands");
+                        const availableBikes = document.getElementById("available-bikes");
+                        const buttonReservation = document.getElementById("validate");
 
                         // Status infos
                         if (station.status === "OPEN") {
@@ -84,18 +76,27 @@ function initMap() {
 
                         if (station.available_bikes < 1) {
                             availableBikes.innerText = "Aucun vélo de disponible à cette station.";
-                            buttonReservation.classList.add('hide');
+                            buttonReservation.classList.add("hide");
                         } else if (station.available_bikes > 0) {
                             availableBikes.innerText = station.available_bikes + " vélo(s) restant(s) disponible(s).";
-                            buttonReservation.classList.remove('hide');
+                            buttonReservation.classList.remove("hide");
                         }
-                        sessionStorage.setItem('stationname', nameTruncated);
-                        sessionStorage.setItem('stationaddress', addressTruncated);
+                        sessionStorage.setItem("stationname", nameTruncated);
+                        sessionStorage.setItem("stationaddress", addressTruncated);
                     });
                 }
             }
 
-        })
+            var stations = JSON.parse(reponse);
+            var lattitude, longitude;
+
+            stations.forEach(function (station) {
+                lattitude = station.position.lat;
+                longitude = station.position.lng;
+                addMarker(station);
+            })
+
+        });
 
 }
 
