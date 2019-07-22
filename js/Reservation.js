8db034cc1420c31a188ed9220fc016533216a3fd
Matthias <m.geslin974@gmail.com>
2019-07-22 09:49:33 +0200

    timer function started -  still not finished

--- js/Reservation.js
+++ js/Reservation.js
@@ -43,6 +43,47 @@ Reservation.prototype.storage = function () {
     let stationName;
     stationName = sessionStorage.getItem("stationname");
 
+    // Time calculator
+    function calculate() {
+        var expiration = sessionStorage.getItem("expiration");
+        var x = setInterval(function() {
+
+            var currentTime = new Date().getTime();
+
+            var gapTime = expiration - currentTime;
+            var timer = sessionStorage.setItem("timer", gapTime);
+
+            // Time calculations for days, hours, minutes and seconds
+            var minutes = Math.floor((gapTime % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((gapTime % (1000 * 60)) / 1000);
+
+
+            let time = document.querySelector(".time");
+            if(gapTime > 0) {
+                time.innerHTML = minutes + "min et " + seconds + "s.</p>" ;
+            }else {
+                clearInterval(x);
+            }
+        } ,1000);
+    }
+
+    var reserve = sessionStorage.getItem("reservationEnabled");
+
+    if (reserve !== true){
+        var gapTime = sessionStorage.getItem("Timer");
+        calculate();
+    }
+
+    let time = document.querySelector(".time");
+    var currentTime = new Date().getTime();
+    var delay = 20 * 60 * 1000;
+    var countDownDate = new Date().getTime() + delay;
+    var expiration = sessionStorage.setItem("expiration", countDownDate);
+    var gapTime = expiration - currentTime;
+
+    var timer = sessionStorage.setItem("timer", gapTime);
+    time.innerText = timer;
+
     document.getElementById("validate").addEventListener("click",function() {
         if ((lastName !== "") && (firstName !== "")) {
             localStorage.setItem("lastname", lastName.value);
