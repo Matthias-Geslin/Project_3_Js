d83f70831e4805688655d2e471bb01147a429125
Matthias <m.geslin974@gmail.com>
2019-07-28 09:00:38 +0200

    rework - timer function edit

--- js/Reservation.js
+++ js/Reservation.js
@@ -6,9 +6,13 @@ class Reservation {
         this.toggleCanvas();
         this.closed();
 
-        this.storage();
+        this.reservation();
+        this.validation();
     }
 }
+
+
+
 
 Reservation.prototype.initReservation = function () {
     const closeIt = document.getElementById("close-it");
@@ -30,70 +34,83 @@ Reservation.prototype.initReservation = function () {
     };
 };
 
-Reservation.prototype.storage = function () {
-    let lastName;
-    lastName = document.getElementById("last-name");
-
-    let firstName;
-    firstName = document.getElementById("first-name");
-
-    let stationAddress;
-    stationAddress = sessionStorage.getItem("stationaddress");
-
-    let stationName;
-    stationName = sessionStorage.getItem("stationname");
-
-    // Time calculator
-    function calculate() {
-        var expiration = sessionStorage.getItem("expiration");
-        var x = setInterval(function() {
-
-            var currentTime = new Date().getTime();
-
-            var gapTime = expiration - currentTime;
-            var timer = sessionStorage.setItem("timer", gapTime);
-
-            // Time calculations for days, hours, minutes and seconds
-            var minutes = Math.floor((gapTime % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((gapTime % (1000 * 60)) / 1000);
 
 
-            let time = document.querySelector(".time");
-            if(gapTime > 0) {
-                time.innerHTML = minutes + "min et " + seconds + "s.</p>" ;
-            }else {
-                clearInterval(x);
-            }
-        } ,1000);
-    }
 
-    var reserve = sessionStorage.getItem("reservationEnabled");
+// Time calculator
+function calculate() {
+    let lastName = document.getElementById("last-name");
+    let firstName = document.getElementById("first-name");
 
-    if (reserve !== true){
-        var gapTime = sessionStorage.getItem("Timer");
-        calculate();
-    }
-
-    let time = document.querySelector(".time");
-    var currentTime = new Date().getTime();
-    var delay = 20 * 60 * 1000;
-    var countDownDate = new Date().getTime() + delay;
-    var expiration = sessionStorage.setItem("expiration", countDownDate);
-    var gapTime = expiration - currentTime;
-
-    var timer = sessionStorage.setItem("timer", gapTime);
-    time.innerText = timer;
-
-    document.getElementById("validate").addEventListener("click",function() {
-        if ((lastName !== "") && (firstName !== "")) {
-            localStorage.setItem("lastname", lastName.value);
-            localStorage.setItem("firstname", firstName.value);
-        }
-    });
+    let stationAddress = sessionStorage.getItem("stationaddress");
+    let stationName = sessionStorage.getItem("stationname");
 
     let storedData = document.getElementById("reservation-data");
-    storedData.innerText = "Vélo réservé à la station " + stationName + ", à l'adresse: " +stationAddress + ". Par " + lastName.value + " " + firstName.value + ". Temps restant: ";
+
+    var expiration = sessionStorage.getItem("expiration");
+
+
+    var x = setInterval(function() {
+
+        var currentTime = new Date().getTime();
+
+        var gapTime = expiration - currentTime;
+        var timer = sessionStorage.setItem("timer", gapTime);
+
+        // Time calculations for days, hours, minutes and seconds
+        var minutes = Math.floor((gapTime % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((gapTime % (1000 * 60)) / 1000);
+
+        if(gapTime > 0) {
+            storedData.innerHTML = "<p>Vélo réservé à la station " + stationName + ", à l'adresse: " +stationAddress + ". Par " + lastName.value + " " + firstName.value + ". Temps restant: " + minutes + "min et " + seconds + "s.</p>" ;
+            sessionStorage.setItem("reservationEnabled", true);
+        }else {
+            sessionStorage.setItem("reservationEnabled", false);
+            clearInterval(x);
+            window.location.reload().sessionStorage.clear();
+            window.alert("Votre session à expirée ainsi que votre réservation.")
+        }
+    } ,1000);
+    localStorage.setItem("last-name", lastName.value);
+    localStorage.setItem("first-name", firstName.value);
+}
+
+
+
+
+Reservation.prototype.reservation = function () {
+    var reservation = sessionStorage.getItem("reservationEnabled");
+    if (reservation === true) {
+        sessionStorage.getItem("timer");
+
+        calculate();
+    }
 };
 
 
 
+
+Reservation.prototype.validation = function () {
+    document.getElementById("validate").addEventListener("click",function() {
+
+        localStorage.getItem("last-name");
+        localStorage.getItem("first-name");
+
+        sessionStorage.setItem("reservationEnabled", true);
+
+        var reserve = sessionStorage.getItem("reservationEnabled");
+
+        var currentTime = new Date().getTime();
+        var delay = 20 * 60 * 1000;
+
+        var countDownDate = new Date().getTime() + delay;
+        var expiration = sessionStorage.setItem("expiration", countDownDate);
+        var gapTime = expiration - currentTime;
+
+        var timer = sessionStorage.setItem("timer", gapTime);
+
+        calculate();
+        document.getElementById("re-booking").classList.remove("hide");
+        document.getElementById("re-booking").classList.add("flex");
+    });
+};
