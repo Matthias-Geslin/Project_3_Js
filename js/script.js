e0b51c1fbf97e88a1df7ae437e8d8ea7eead825c
Matthias <m.geslin974@gmail.com>
2019-08-12 09:45:41 +0200

    init functions added

--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@
 document.addEventListener("DOMContentLoaded", function (e) {
     // Create and launch the slider
     var slider = new Slider();
+    slider.begin();
 
     var map = new Map();
 
     var reservation = new Reservation();
+    reservation.initReservation();
 
     var canvas = new Canvas();
+    canvas.initCanvas();
+
 });
