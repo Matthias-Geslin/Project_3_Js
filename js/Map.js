e705a9119dd8d5f292f91107978bd25f42f71f1e
Matthias <m.geslin974@gmail.com>
2019-08-17 13:17:56 +0200

    half fixed map - rework in class, renamed - marker listener need changes

--- js/Map.js
+++ js/Map.js
@@ -1,109 +1,101 @@
 "use strict";
 
-class Map {
+class GoogleMap {
     constructor(){
-        this.addMarker();
-        this.init();
+        this.nantes = {lat: 47.2173, lng: -1.5534};
     }
-}
 
-Map.prototype.init = function initMap() {
-    var maps = new google.maps.Map(document.getElementById("map"), {
-        zoom: 13,
-        center: {lat: 47.2173, lng: -1.5534}
-    });
+    initMap() {
+        var maps = new google.maps.Map(document.getElementById("map"), {
+            zoom: 13,
+            center: this.nantes
+        });
 
-    ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Nantes&apiKey=58866263d27fa1a6d54be4b8a79069f811cd87fd",
-        function (reponse) {
+        ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Nantes&apiKey=58866263d27fa1a6d54be4b8a79069f811cd87fd",
+            function (reponse) {
+                var stations = JSON.parse(reponse);
+                var latitude, longitude;
+                // Add Markers for each station latitude & longitude
+                stations.forEach(function (station) {
+                    latitude = station.position.lat;
+                    longitude = station.position.lng;
+                    addMarker(station);
+                });
 
 
-            var stations = JSON.parse(reponse);
-            var lattitude, longitude;
-            // Add Markers for each station latitude & longitude
-            stations.forEach(function (station) {
-                lattitude = station.position.lat;
-                longitude = station.position.lng;
-                this.addMarker(station);
+                function addMarker(station) {
+                    let stationStatus = document.getElementById("station-status");
+
+                    var marker = new google.maps.Marker({
+                        position: station.position,
+                        map: maps,
+                        icon:""
+                    })
+                    // Check if station Open
+                    if (station.status === "OPEN") {
+                        marker.icon= {
+                            url: "img/green.png",
+                        };
+                        stationStatus.textContent = "Ouverte";
+                    } else {
+                        marker.icon= {
+                            url:"img/red.png"
+                        };
+                        stationStatus.textContent = "Fermée";
+                    }
+
+                    // Check content
+                    if (station.name) {
+                        var infoWindow = new google.maps.InfoWindow({
+                            content: station.name
+                        });
+                        const reserve = document.getElementById("reservation-box");
+
+                        // marker.addEventListener("click", function () {
+                        //
+                        //     reserve.classList.remove("hide");
+                        //     reserve.classList.add("flex");
+                        //
+                        //     const stationName = document.getElementById("station-name");
+                        //     const stationAddress = document.getElementById("station-address");
+                        //     const bikeStands = document.getElementById("station-stands");
+                        //     const availableBikes = document.getElementById("available-bikes");
+                        //     const buttonReservation = document.getElementById("validate");
+                        //
+                        //     // Status infos
+                        //     if (station.status === "OPEN") {
+                        //         stationStatus.textContent = "Ouverte";
+                        //     } else {
+                        //         stationStatus.textContent = "Fermée";
+                        //     }
+                        //
+                        //     // Removing excessive numbers with regex
+                        //     const regex = /#0+0/gm;
+                        //     const nameString = `${station.name}`;
+                        //     const addressString = `${station.address}`;
+                        //     const subst = "";
+                        //
+                        //     // The substituted value will be contained in the result variable
+                        //     const nameTruncated = nameString.replace(regex, subst);
+                        //     const addressTruncated = addressString.replace(regex, subst);
+                        //
+                        //     stationName.innerText = "Nom: " + nameTruncated;
+                        //     stationAddress.innerText = "Adresse: " + addressTruncated;
+                        //     bikeStands.innerText = station.bike_stands + " supports à vélo.";
+                        //
+                        //     if (station.available_bikes < 1) {
+                        //         availableBikes.innerText = "Aucun vélo de disponible à cette station.";
+                        //         buttonReservation.classList.add("hide");
+                        //     } else if (station.available_bikes > 0) {
+                        //         availableBikes.innerText = station.available_bikes + " vélo(s) restant(s) disponible(s).";
+                        //         buttonReservation.classList.remove("hide");
+                        //     }
+                        //     sessionStorage.setItem("stationname", nameTruncated);
+                        //     sessionStorage.setItem("stationaddress", addressTruncated);
+                        // });
+                    }
+
+                }
             });
-        });
+    };
 };
-
-
-
-
-// Add Marker Function
-Map.prototype.addMarker = function(station) {
-    var marker = new google.maps.Marker({
-        position: station.position,
-        map: maps,
-        icon:""
-    });
-
-    let stationStatus = document.getElementById("station-status");
-    // Check if station Open
-    if (station.status === "OPEN") {
-        marker.icon= {
-            url: "img/green.png",
-        };
-        stationStatus.textContent = "Ouverte";
-    } else {
-        marker.icon= {
-            url:"img/red.png"
-        };
-        stationStatus.textContent = "Fermée";
-    }
-
-
-
-    // Check content
-    if (station.name) {
-        var infoWindow = new google.maps.InfoWindow({
-            content: station.name
-        });
-        const reserve = document.getElementById("reservation-box");
-
-        marker.addListener("click", function () {
-
-            reserve.classList.remove("hide");
-            reserve.classList.add("flex");
-
-            const stationName = document.getElementById("station-name");
-            const stationAddress = document.getElementById("station-address");
-            const bikeStands = document.getElementById("station-stands");
-            const availableBikes = document.getElementById("available-bikes");
-            const buttonReservation = document.getElementById("validate");
-
-            // Status infos
-            if (station.status === "OPEN") {
-                stationStatus.textContent = "Ouverte";
-            } else {
-                stationStatus.textContent = "Fermée";
-            }
-
-            // Removing excessive numbers with regex
-            const regex = /#0+0/gm;
-            const nameString = `${station.name}`;
-            const addressString = `${station.address}`;
-            const subst = "";
-
-            // The substituted value will be contained in the result variable
-            const nameTruncated = nameString.replace(regex, subst);
-            const addressTruncated = addressString.replace(regex, subst);
-
-            stationName.innerText = "Nom: " + nameTruncated;
-            stationAddress.innerText = "Adresse: " + addressTruncated;
-            bikeStands.innerText = station.bike_stands + " supports à vélo.";
-
-            if (station.available_bikes < 1) {
-                availableBikes.innerText = "Aucun vélo de disponible à cette station.";
-                buttonReservation.classList.add("hide");
-            } else if (station.available_bikes > 0) {
-                availableBikes.innerText = station.available_bikes + " vélo(s) restant(s) disponible(s).";
-                buttonReservation.classList.remove("hide");
-            }
-            sessionStorage.setItem("stationname", nameTruncated);
-            sessionStorage.setItem("stationaddress", addressTruncated);
-        });
-    }
-};
-
