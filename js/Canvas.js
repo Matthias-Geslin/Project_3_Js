47eb01588d5a563e16b9e085da993a2e9f1ad489
Matthias <m.geslin974@gmail.com>
2019-08-19 10:00:17 +0200

    canvas edit - functions , listeners

--- js/Canvas.js
+++ js/Canvas.js
@@ -4,17 +4,12 @@ class Canvas {
     constructor() {
         this.enable();
         this.disable();
-
-        this.canvas = document.getElementById("canvas");
-
-        this.lastName = document.getElementById("last-name");
-        this.firstName = document.getElementById("first-name");
-        this.buttonReservation = document.getElementById("validate");
     }
 }
 
 Canvas.prototype.initCanvas = function () {
-    var ctx = this.canvas.getContext("2d");
+    let canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext("2d");
 
 // define the type of the draw pointer
     ctx.lineJoin = "round";
@@ -40,36 +35,44 @@ Canvas.prototype.initCanvas = function () {
         [lastX, lastY] = [X, Y];
     }
 
-    this.canvas.addEventListener("mousedown", (e) => {
+    canvas.addEventListener("mousedown", (e) => {
         isDrawing = true;
         [lastX, lastY] = [e.layerX, e.layerY];
     });
-    this.canvas.addEventListener("mousemove", (e) => draw(e.layerX, e.layerY));
-    this.canvas.addEventListener("mouseup", () => isDrawing = false);
-    this.canvas.addEventListener("mouseout", () => isDrawing = false);
+    canvas.addEventListener("mousemove", (e) => draw(e.layerX, e.layerY));
+    canvas.addEventListener("mouseup", () => isDrawing = false);
+    canvas.addEventListener("mouseout", () => isDrawing = false);
 };
 
 
 
 Canvas.prototype.enable = function () {
+    let canvas = document.getElementById("canvas");
+    let lastName = document.getElementById("last-name");
+    let firstName = document.getElementById("first-name");
+    let buttonReservation = document.getElementById("validate");
+
     // Enabling the validate button
     function enable() {
-        this.buttonReservation.removeAttribute("disabled");
+        buttonReservation.removeAttribute("disabled");
     }
-    this.canvas.addEventListener("mousedown", function () {
-        if (this.lastName.value && this.firstName.value !== "") {
-            setTimeout(enable,4000);
+    canvas.addEventListener("mousedown", function () {
+        if (lastName.value && firstName.value !== "") {
+            setTimeout(enable,2000);
         }
     });
 };
 
 
 Canvas.prototype.disable = function () {
+    let canvas = document.getElementById("canvas");
+    let buttonReservation = document.getElementById("validate");
+
     // Disabling the validate button
     function disable() {
-        this.buttonReservation.setAttribute("disabled","");
+        buttonReservation.setAttribute("disabled","");
     }
-    this.canvas.addEventListener("mouseout", function () {
-        setTimeout(disable,5000);
+    canvas.addEventListener("mouseout", function () {
+        setTimeout(disable,10000);
     });
 };
